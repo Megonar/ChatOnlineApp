83e809a8d6216553fa4ea337987bc9e1ba9523e6
Dmitry <raman0vichdzmitry123@gmail.com>
2023-10-15 22:15:29 +0300

    fix message controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,26 +3,35 @@ module Api
     class MessagesController < ApplicationController
       skip_before_action :verify_authenticity_token
 
-      def create
-        @chat = Chat.find_by(title: message_params[:chat_title])
-        @message = @chat.messages.new(text: message_params[:text], user: anonymous_user)
+      before_action :find_chat, only: :create
 
-        if @message.save
-          @message.broadcast_append_to(@chat, target: "chat_#{@chat.id}_messages", locals: { user: anonymous_user, message: @message })
-          render json: { message: "Message successfully created" }, status: :created
+      def create
+        if @chat
+          @message = @chat.messages.new(text: message_params[:text], user: anonymous_user)
+
+          if @message.save
+            @message.broadcast_append_to(@chat, target: "chat_#{@chat.id}_messages", locals: { user: anonymous_user, message: @message })
+            render json: { message: "Message successfully created" }, status: :created
+          else
+            render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+          end
         else
-          render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: "Chat not found" }, status: :not_found
         end
       end
 
       private
+
+      def find_chat
+        @chat = Chat.find_by(title: params[:chat_id])
+      end
 
       def anonymous_user
         User.find_by(email: 'default_user@example.com')
       end
 
       def message_params
-        params.permit(:chat_title, :text)
+        params.permit(:text)
       end
     end
   end
